ff7b24f13fb1bfffc386db9dd9d457e60cdfba5c
wangang.wa <wangang.wa@alibaba-inc.com>
2024-08-02 16:45:43 +0800

    update config.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,6 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import datetime
-import torchacc
 
 project = 'PAI-TorchAcc'
 copyright = '2024, alibaba-cloud'
