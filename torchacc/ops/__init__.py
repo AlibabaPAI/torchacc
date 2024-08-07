d4d95dac4b3691197c521c98e5c717b40d696775
luzhan <luzhan.lz@alibaba-inc.com>
2024-08-07 18:14:49 +0800

    feat: add spmd flash attention op

--- torchacc/ops/__init__.py
+++ torchacc/ops/__init__.py
@@ -1,3 +1,3 @@
-from .flash_attn import (flash_attn_varlen_qkvpacked_xla, flash_attn_varlen_xla,
+from .flash_attn import (flash_attn_varlen_qkvpacked_xla, flash_attn_varlen_xla, spmd_flash_attn_varlen_xla,
                          flash_attn_xla)
 from .scaled_dot_product_attention import scaled_dot_product_attention
