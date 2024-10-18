e4bec1bec9d58d67a3886586c1c82c2498c385fb
shw <shw443569@alibaba-inc.com>
2024-10-18 15:54:37 +0800

    format

--- torchacc/dist/__init__.py
+++ torchacc/dist/__init__.py
@@ -5,22 +5,17 @@ import torch
 import torch.distributed as dist
 import torch_xla
 import torch_xla.core.xla_model as xm
-
 import torchacc as ta
 
 # register lazy backend
-from . import backend
-
-from .mesh import Mesh
-
-from .parallel_module import ParallelModule
+from . import backend, fsdp, pp, tp
+from .distributed_parallel import DistributedParallel
 from .dp import DataParallel
 from .fsdp import FullyShardedDataParallel
-from .spmd_fsdp import SpmdFullyShardedDataParallel
+from .mesh import Mesh
+from .parallel_module import ParallelModule
 from .pp import PipelineParallel
-from .distributed_parallel import DistributedParallel
-
-from . import fsdp, pp, tp
+from .spmd_fsdp import SpmdFullyShardedDataParallel
 
 BACKEND_NAME = backend._BACKEND_NAME
 EAGER_BACKEND_NAME = backend._EAGER_BACKEND_NAME
