1dbc3b61c4f2eeea53df4d24e56f79caac9eb482
Yitong Huang <huangyitong.hyt@alibaba-inc.com>
2024-10-21 16:55:26 +0800

    add ut to test

--- tests/run_ut.sh
+++ tests/run_ut.sh
@@ -6,6 +6,7 @@ export PYTHONPATH=$PYTHONPATH:.
 
 function test_unittests() {
     pytest tests/core/test_bucketing_loader.py
+    pytest tests/core/test_dynamic.py
     pytest tests/distributed/test_dist_ops.py
     pytest tests/ops/test_flash_attn.py
     pytest tests/ops/test_context_parallel.py
