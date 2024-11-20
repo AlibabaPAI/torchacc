a8500126bb397efa306112c1df7104581a7d17ec
Seventeen17 <17aloha@gmail.com>
2024-11-20 19:12:47 +0800

    fix context parallel tests

--- torchacc/ops/scaled_dot_product_attention.py
+++ torchacc/ops/scaled_dot_product_attention.py
@@ -1,7 +1,7 @@
 import einops
 import torch
 
-from .flash_attn import flash_attn_varlen_xla
+from .flash_attn import flash_attn_xla
 
 
 def scaled_dot_product_attention(query,
@@ -13,27 +13,13 @@ def scaled_dot_product_attention(query,
                                  scale=None) -> torch.Tensor:
     # TODO: shape check and scale support
     bsz = query.shape[0]
-    q_len = query.shape[-2]
-    query = einops.rearrange(query, "b h s ... -> (b s) h ...")
-    key = einops.rearrange(key, "b h s ... -> (b s) h ...")
-    value = einops.rearrange(value, "b h s ... -> (b s) h ...")
-    cu_lens = torch.arange(
-        0, (bsz + 1) * q_len,
-        step=q_len,
-        dtype=torch.int32,
-        device=query.device)
-    max_s = q_len
+
     is_causal = attn_mask is not None or is_causal
-    output = flash_attn_varlen_xla(
+    output = flash_attn_xla(
         query,
         key,
         value,
-        cu_lens,
-        cu_lens,
-        max_s,
-        max_s,
         dropout_p,
         softmax_scale=None,
         causal=is_causal)
-    output = einops.rearrange(output, "(b s) h ... -> b h s ...", b=bsz)
     return output
