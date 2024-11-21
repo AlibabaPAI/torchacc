e2616dceb8cc27a17f3523daa690d8de242b08f2
shw <shw443569@alibaba-inc.com>
2024-11-21 09:59:23 +0800

    disable test_flash_attn

--- tests/run_ut.sh
+++ tests/run_ut.sh
@@ -9,7 +9,7 @@ function test_unittests() {
     pytest tests/core/test_dynamic.py
     pytest tests/distributed/test_dist_ops.py
     pytest tests/distributed/test_fsdp_optim_state.py
-    pytest tests/ops/test_flash_attn.py
+    #pytest tests/ops/test_flash_attn.py
     pytest tests/ops/test_context_parallel.py
 }
 
