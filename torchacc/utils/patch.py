a9db601e39b69e0bb42f9aaddcd30eb9d5fc4aa8
Tianxing Wang <55205022+shadow150519@users.noreply.github.com>
2024-11-22 16:33:12 +0800

    Add qwen patch (#33)
    * add qwen patch

--- torchacc/utils/patch.py
+++ torchacc/utils/patch.py
@@ -59,11 +59,12 @@ def patch_fa():
     Replace `transformers.modeling_flash_attention_utils._flash_attention_forward` with
     `torchacc.ops.flash_attn_xla` and `torchacc.ops.flash_attn_varlen_xla`
     '''
+    from .logger import logger
     try:
         import transformers
         from packaging import version
         version_ts = transformers.__version__
-        if version.parse(version_ts) >= version.parse("4.42.0"):
+        if version.parse(version_ts) >= version.parse("4.43.0"):
             import transformers.modeling_flash_attention_utils as modeling_flash_attention_utils
             from typing import Optional
 
@@ -111,8 +112,9 @@ def patch_fa():
                     flash_attn.flash_attn_func, ops.flash_attn_xla)
             except ImportError:
                 logger.warn(f"Patch flash_attn.flash_attn_func failed.")
-    except:
-        logger.warn('torchacc will not patch any FlashAttention function.')
+    except Exception as e:
+        logger.warn(
+            f'torchacc will not patch any FlashAttention function due to {e}.')
 
 
 def patch_llama(use_flash_attn):
@@ -138,3 +140,58 @@ def patch_llama(use_flash_attn):
             return None
 
         LlamaModel._update_causal_mask = update_causal_mask
+
+
+def patch_qwen(use_flash_attn):
+    '''
+    Modify the calculation of `rotary_seq_len` in `Qwen2FlashAttention2.forward` to avoid xla graph be executed.
+    Replace `transformers.models.qwen.modeling_qwen2.Qwen2Model._update_causal_mask` with `return None` 
+    and replace flash_attn with the interface in torchacc. This requires transformers>=4.41.0.
+    '''
+    import inspect
+    import transformers
+    from .logger import logger
+    from packaging import version
+
+    if use_flash_attn:
+        from transformers.cache_utils import Cache
+        from transformers.models.qwen2.modeling_qwen2 import Qwen2Model
+
+        def update_causal_mask(
+            self,
+            attention_mask: torch.Tensor,
+            input_tensor: torch.Tensor,
+            cache_position: torch.Tensor,
+            past_key_values: Cache,
+            output_attentions: bool,
+        ):
+            if attention_mask is not None:
+                return attention_mask
+            return None
+
+        Qwen2Model._update_causal_mask = update_causal_mask
+
+    if version.parse(transformers.__version__) >= version.parse("4.37.0"):
+        try:
+            import transformers.models.qwen2.modeling_qwen2 as qwen2
+            import re
+
+            src = inspect.getsource(qwen2.Qwen2FlashAttention2)
+
+            pattern1 = r"rotary_seq_len\s*=\s*\(\s*max\(kv_seq_len,\s*position_ids\[:,\s*-1\]\.max\(\)\.item\(\)\s*\+\s*1\)\s*if\s*position_ids\s*is\s*not\s*None\s*else\s*kv_seq_len\s*\)"
+            pattern2 = r"rotary_seq_len\s*=\s*max\(kv_seq_len,\s*position_ids\[:,\s*-1\]\.max\(\)\.item\(\)\)\s*\+\s*1"
+            replacement = "rotary_seq_len = kv_seq_len"
+
+            src = re.sub(pattern1, replacement, src)
+            src = re.sub(pattern2, replacement, src)
+            dict_src = \
+            """\nQWEN2_ATTENTION_CLASSES = {
+            "eager": Qwen2Attention,
+            "flash_attention_2": Qwen2FlashAttention2,
+            "sdpa": Qwen2SdpaAttention,
+            }
+            """
+            src = src + dict_src
+            exec(src, qwen2.__dict__)
+        except Exception as e:
+            logger.warning(f"patch qwen2 failed due to: {e}")
