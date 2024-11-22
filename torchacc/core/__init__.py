cef2a0ad6d0d8537d3352d6cef65ac3391dbc2d7
shw <shw443569@alibaba-inc.com>
2024-11-22 14:11:41 +0800

    raise error for lazy mode without torch_xla

--- torchacc/core/__init__.py
+++ torchacc/core/__init__.py
@@ -12,6 +12,8 @@ if is_torch_xla_available():
 
 
 def lazy_device():
+    if not is_torch_xla_available():
+        raise NotImplemented('Lazy device require torch_xla environment')
     device = xm.xla_device()
     xm.set_replication(device, [device])
     return device
