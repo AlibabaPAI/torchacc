1664e17dd31f01e4905cd849c513afdc31fb2ecb
wangang.wa <wangang.wa@alibaba-inc.com>
2024-12-06 14:25:42 +0800

    Use llama.sh instead of llama_acc.sh and llama_torch.sh

--- benchmarks/accuracy_benchmark/run_accuracy_bench.sh
+++ benchmarks/accuracy_benchmark/run_accuracy_bench.sh
@@ -10,45 +10,45 @@ MODEL_DIR=$(realpath "$1")
 MODEL_NAME=$(basename "$MODEL_DIR")
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 RES_FOLDER="./result/$TIMESTAMP"
-MODEL_NAME_TORCH="$MODEL_NAME"_torch
-MODEL_NAME_ACC="$MODEL_NAME"_acc
+MODEL_NAME_TORCH="$RES_FOLDER/torch_ckpt"
+MODEL_NAME_ACC="$RES_FOLDER/acc_ckpt"
 TORCH_TRAIN_LOG="$RES_FOLDER/torch_training.log"
 ACC_TRAIN_LOG="$RES_FOLDER/acc_training.log"
 ORIG_MODEL_EVAL_LOG="$RES_FOLDER/original_model_eval.log"
 TORCH_MODEL_EVAL_LOG="$RES_FOLDER/torch_model_eval.log"
 ACC_MODEL_EVAL_LOG="$RES_FOLDER/acc_model_eval.log"
+RES_LOG_FILE="$RES_FOLDER/result.log"
 
 mkdir -p $RES_FOLDER
 
-##### Reinstall transformers #####
-git clone https://github.com/huggingface/transformers.git 2>/dev/null || echo "Directory already exists, skipping clone."
-pushd transformers
-TRANSFORMERS_DIR=$(realpath "./")
-if ! pip list 2>/dev/null | grep -q "$TRANSFORMERS_DIR"; then
-  pip uninstall -y transformers
-  pip install -e .
-fi
-popd
 
-##### Run the torch native job ######
-bash ./llama_torch.sh "$TRANSFORMERS_DIR" "$MODEL_DIR" 2>&1 | tee $TORCH_TRAIN_LOG
+# Run the torch native job
+bash ./llama.sh "$MODEL_DIR" 0 $MODEL_NAME_TORCH 2>&1 | tee $TORCH_TRAIN_LOG
 
-##### Run the torchacc job ######
-bash ./llama_acc.sh "$TRANSFORMERS_DIR" "$MODEL_DIR" 2>&1 | tee $ACC_TRAIN_LOG
+# Run the torchacc job
+bash ./llama.sh "$MODEL_DIR" 1 $MODEL_NAME_ACC 2>&1 | tee $ACC_TRAIN_LOG
 
-##### Evaluate original job ######
+# Evaluate original checkpoint
 bash ./mtbench.sh "$MODEL_DIR" 2>&1 | tee $ORIG_MODEL_EVAL_LOG
 
-##### Evaluate Torch job ######
-bash ./mtbench.sh "./$MODEL_NAME_TORCH" 2>&1 | tee $TORCH_MODEL_EVAL_LOG
+# Evaluate Torch job
+bash ./mtbench.sh "$MODEL_NAME_TORCH" 2>&1 | tee $TORCH_MODEL_EVAL_LOG
 
-##### Evaluate TorchAcc job ######
-bash ./mtbench.sh "./$MODEL_NAME_ACC" 2>&1 | tee $ACC_MODEL_EVAL_LOG
+# Evaluate TorchAcc job
+bash ./mtbench.sh "$MODEL_NAME_ACC" 2>&1 | tee $ACC_MODEL_EVAL_LOG
 
-##### Collect and compre the result ######
+# Collect and compare the results
 ORIG_SCORE=$(tail -1 $ORIG_MODEL_EVAL_LOG | awk '{print $NF}')
 TORCH_SCORE=$(tail -1 $TORCH_MODEL_EVAL_LOG | awk '{print $NF}')
 ACC_SCORE=$(tail -1 $ACC_MODEL_EVAL_LOG | awk '{print $NF}')
+
+torch_train_loss=$(grep -oP 'train_loss\s*=\s*\K[0-9.]*' $TORCH_TRAIN_LOG)
+torch_train_runtime=$(grep -oP 'train_runtime\s*=\s*\K[0-9:.]*' $TORCH_TRAIN_LOG)
+torch_train_samples_per_second=$(grep -oP 'train_samples_per_second\s*=\s*\K[0-9.]*' $TORCH_TRAIN_LOG)
+acc_train_loss=$(grep -oP 'train_loss\s*=\s*\K[0-9.]*' $ACC_TRAIN_LOG)
+acc_train_runtime=$(grep -oP 'train_runtime\s*=\s*\K[0-9:.]*' $ACC_TRAIN_LOG)
+acc_train_samples_per_second=$(grep -oP 'train_samples_per_second\s*=\s*\K[0-9.]*' $ACC_TRAIN_LOG)
+
 
 RESET='\033[0m'
 RED='\033[31m'
@@ -57,10 +57,21 @@ YELLOW='\033[33m'
 BLUE='\033[34m'
 CYAN='\033[36m'
 
-# Print colored output
-echo -e "${BLUE}==================== Final Results ====================${RESET}"
-echo -e "${YELLOW}Original Model Score        : ${GREEN}${ORIG_SCORE}${RESET}"
-echo -e "${YELLOW}Torch Model Score           : ${GREEN}${TORCH_SCORE}${RESET}"
-echo -e "${YELLOW}TorchAcc Model Score        : ${GREEN}${ACC_SCORE}${RESET}"
-echo -e "\n${CYAN}More details can be found in: ${RESET}${RES_FOLDER}"
-echo -e "${BLUE}=======================================================${RESET}"
+{
+  echo -e "${BLUE}==================== Training Results ====================${RESET}"
+  echo -e "${YELLOW}Torch train loss                  = ${GREEN}${torch_train_loss}${RESET}"
+  echo -e "${YELLOW}TorchAcc train loss               = ${GREEN}${acc_train_loss}${RESET}"
+  echo -e "${YELLOW}Torch train runtime               = ${GREEN}${torch_train_runtime}${RESET}"
+  echo -e "${YELLOW}TorchAcc train runtime            = ${GREEN}${acc_train_runtime}${RESET}"
+  echo -e "${YELLOW}Torch train samples per second    = ${GREEN}${torch_train_samples_per_second}${RESET}"
+  echo -e "${YELLOW}TorchAcc train samples per second = ${GREEN}${acc_train_samples_per_second}${RESET}"
+
+
+  echo -e "${BLUE}==================== Evaluate Results ====================${RESET}"
+  echo -e "${YELLOW}Original Model Score         = ${GREEN}${ORIG_SCORE}${RESET}"
+  echo -e "${YELLOW}Torch Model Score            = ${GREEN}${TORCH_SCORE}${RESET}"
+  echo -e "${YELLOW}TorchAcc Model Score         = ${GREEN}${ACC_SCORE}${RESET}"
+
+  echo -e "\n${CYAN}More details can be found in = ${RESET}${RES_FOLDER}"
+  echo -e "${BLUE}==========================================================${RESET}"
+} | tee >(sed 's/\x1b\[[0-9;]*m//g' > $RES_LOG_FILE)
