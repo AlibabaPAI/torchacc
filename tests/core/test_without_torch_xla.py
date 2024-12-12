cd67d313ed18c28216df1b4ad0705b5a81023beb
Ang Wang <wangang.wa@alibaba-inc.com>
2024-12-12 15:17:12 +0800

    Optimize the workflow and unit test (#40)

--- tests/core/test_without_torch_xla.py
+++ tests/core/test_without_torch_xla.py
@@ -1,9 +1,15 @@
-import sys
+import subprocess
 
 
 def test_torchacc_without_torch_xla():
-    # disable the torch_xla in environment
-    sys.modules["torch_xla"] = None
-    from torchacc.utils.import_utils import is_torch_xla_available
-    assert is_torch_xla_available() == False
-    import torchacc
+    python_code = """
+import sys
+sys.modules["torch_xla"] = None
+from torchacc.utils.import_utils import is_torch_xla_available
+assert is_torch_xla_available() == False
+import torchacc
+    """
+    command = ['python', '-c', python_code]
+    # To avoid being affected by imports from other tests,
+    # this testing is done here in the subprocess.
+    result = subprocess.run(command, check=True, text=True)
